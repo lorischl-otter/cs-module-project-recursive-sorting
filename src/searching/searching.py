5c708dcc7b5ae2f0dd360e61c0843522deb09f9f
Lori Schlatter <lori.schlatter@gmail.com>
2020-07-22 15:37:36 -0500

    Completed module 2 assignments with passing tests.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,67 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # if empty array, return -1
+    if start > end:
+        return -1
+    # once start and end pointing at same number, must return something
+    if start == end:
+        # if that number is the target, return that index
+        if arr[start] == target:
+            return start
+        # otherwise, target not in list
+        else:
+            return -1
 
+    mid = (start + end) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search(arr, target, start, mid-1)
+    else:
+        return binary_search(arr, target, mid+1, end)
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+
+# Solution implemented in class
+def descending_binary_search(arr, target, start, end):
+    # coded in class
+    if start > end:
+        return -1
+    # if empty array, return -1
+    else:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            return descending_binary_search(arr, target, start, mid-1)
+        else:
+            return descending_binary_search(arr, target, mid+1, end)
+
+
+arr = [101, 99, 87, 76, 66, 65]
+print(descending_binary_search(arr, 67, 0, len(arr)-1))
+
+
+def agnostic_binary_search(arr, target):
+    # figure out whether input array is ascending or descending
+    # keep boolean to indicate order
+    if arr[0] > arr[-1]:
+        is_ascending = False
+    else:
+        is_ascending = True
+
+    # if ascending, call 'binary_search'
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    # otherwise, call 'descending_binary_search'
+    else:
+        return descending_binary_search(arr, target, 0, len(arr) - 1)
+
+
+print(agnostic_binary_search(arr, 67))
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,79 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = []
 
-    # Your code here
+    # create pointers
+    a = 0
+    b = 0
 
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+
+    if a < len(arrA):
+        merged_arr.extend(arrA[a:])
+
+    if b < len(arrB):
+        merged_arr.extend(arrB[b:])
 
     return merged_arr
+    # Incomplete attempt before class
+
+    # num_elements = len(arrA) + len(arrB)
+    # merged_arr = []
+    # while num_elements > 0:
+    #     if arrA[0] < arrB[0]:
+    #         merged_arr.append(arrA[0])
+    #         arrA.pop(0)
+    #         num_elements -= 1
+    #     else:
+    #         merged_arr.append(arrB[0])
+    #         arrB.pop(0)
+    #         num_elements -= 1
+    # return merged_arr
+
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
+    if len(arr) > 1:
+        left = merge_sort(arr[0: len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+    # Incomplete attempt before class
+    # # if array is empty or only contains one item, return array
+    # if len(arr) <= 1:
+    #     return arr
+    # # if array is 2 elements, utilize merge function
+    # elif len(arr) == 2:
+    #     return merge(, arr[1])
+    # # if array is over 2 elements, continue recursive splitting
+    # else:
+    #     start = 0
+    #     end = len(arr) - 1
+    #     mid = len(arr) // 2
+    #     return merge_sort(arr[start:mid]) + merge_sort(arr[mid:end])
+    #     # call merge_sort on both halves
+
+    # return arr
+
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
-def merge_sort_in_place(arr, l, r):
+def merge_sort_in_place(arr, left, right):
     # Your code here
-
+    pass
